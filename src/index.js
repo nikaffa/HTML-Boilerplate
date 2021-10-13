20e26bea376dec49638adbe50b3d5062951190e0
Veronika Fatikhova <63478887+nikaffa@users.noreply.github.com>
2021-10-12 17:30:02 -0700

    empty file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
-import {styleBody, addTitle, contact} from './dom'; //imports functions from dom.js
-import users, { getPremiumUsers } from './data'; //import default value and function
 
-const premUsers = getPremiumUsers(users);
-console.log(users, premUsers);
-
-console.log('test 2')
